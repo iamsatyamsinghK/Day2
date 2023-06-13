25ac408292374ac652343bf9c9a4f3dd133d5f72
iamsatyamsinghK <satyamsinghkushwah162@gmail.com>
2023-06-13 13:37:07 +0530

    Initial Create

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Day2;
+class myCircle
+{
+    public double radius;
+    public myCircle(double radius)
+    {
+        this.radius = radius;
+    }
+    public double GetArea()
+    {
+        double area;
+        area = 3.14 * radius * radius;
+        return area;
+    }
+    public double GetCircumference()
+    {
+        double circ;
+        circ = 2 * 3.14 * radius;
+        return circ;
+    }
+    public static void Main()
+    {
+        myCircle ci = new myCircle(5);
+        Console.WriteLine("Area of Circle is :" + ci.GetArea());
+        Console.WriteLine("Circumference of Circle is :" + ci.GetCircumference());
+    }
+}
